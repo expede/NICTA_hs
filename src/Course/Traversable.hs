a11d1f9ddb516cb6502418b37100310626956be7
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-01-02 15:55:25 -0800

    Changed layout for  and

--- src/Course/Traversable.hs
+++ src/Course/Traversable.hs
@@ -10,17 +10,14 @@ import Course.Applicative
 import Course.List
 
 class Functor t => Traversable t where
-  traverse ::
-    Applicative f =>
-    (a -> f b)
-    -> t a
-    -> f (t b)
+  traverse :: Applicative f =>
+             (a -> f b)
+           -> t a
+           -> f (t b)
 
 instance Traversable List where
-  traverse ::
-    Applicative f =>
-    (a -> f b)
-    -> List a
-    -> f (List b)
-  traverse f =
-    foldRight (\a b -> (:.) <$> f a <*> b) (pure Nil)
+  traverse :: Applicative f =>
+             (a -> f b)
+           -> List a
+           -> f (List b)
+  traverse f = foldRight (\a b -> (:.) <$> f a <*> b) (pure Nil)
